1c1bd0781dddc8c079d8e11e8040897e329de3d5
hdungie <120076145+hdungie@users.noreply.github.com>
2023-04-07 15:02:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ region = st.selectbox(
      'Choose a region?',
      ('Nasdaq', 'Vietnam'))
 
- st.write('You selected:', option)
+st.write('You selected:', option)
 company = st.text_input("Search companies by name or ticker", value="")
