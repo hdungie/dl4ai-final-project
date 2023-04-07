1bb0008cb5e4ecc021a3cfbd2b40b22161412107
hdungie <120076145+hdungie@users.noreply.github.com>
2023-04-07 15:11:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 region = st.selectbox('Choose a region?', ('Nasdaq', 'Vietnam'))
 
-interval = st.slider('Choose an interval', min_value = 1, max_value = 365, step = 1)
-
 company = st.text_input("Search companies by name or ticker", value="")
+
+interval = st.slider('Choose an interval', min_value = 1, max_value = 365, step = 1)
