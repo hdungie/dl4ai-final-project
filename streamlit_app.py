b0f42e991c7cd7acc2ce1c757984f5254ba70061
hdungie <120076145+hdungie@users.noreply.github.com>
2023-04-14 12:23:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,6 @@ col1, col2 = st.columns([1,3])
 with col1: region = st.selectbox('Choose a region?', ('Nasdaq', 'Vietnam'))
 with col2: company = st.text_input("Search companies by name or ticker", value="")
 
-interval = st.slider('Choose an interval', min_value = 1, max_value = 365, step = 1)
-
 col1, col2 = st.columns(2)
 with col1: start_date = st.date_input( "Start date: ")
 with col2: end_date = st.date_input("End date: ")
