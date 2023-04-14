17a2db5ca4da59d5bfa682b692d72231d9036525
hdungie <120076145+hdungie@users.noreply.github.com>
2023-04-14 16:09:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ if region == "Nasdaq":
 else: 
   df = pd.read_csv('./search_engine_vn.csv')
   
-text_search = text_search.lower()
+company = company.lower()
 df = df.apply(lambda x: x.str.lower() if x.dtype == "object" else x)
 
 m1 = df['symbol'].str.contains(company)
