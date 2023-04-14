f1bc080e458c3d2e972c1608833da7fde28ac588
hdungie <120076145+hdungie@users.noreply.github.com>
2023-04-14 16:59:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,12 +11,8 @@ else:
   df = pd.read_csv('./search_engine_vn.csv')
   
 df_search = df['company']
-if st.session_state.get('user_input') is None:
-    with col2: user_input = st.text_input("Enter your search query:")
-    st.session_state['user_input'] = user_input
-else:
-    with col2:
-        company = st.selectbox("Search companies by name or symbol", df_search)
+with col2:
+  company = st.selectbox("Search companies by name or symbol", df_search)
 
 col1, col2 = st.columns(2)
 with col1: start_date = st.date_input( "Start date: ")
