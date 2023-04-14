12b43de3bb3529361d3e1abd99e6fcc932b17807
hdungie <120076145+hdungie@users.noreply.github.com>
2023-04-14 16:12:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 df_search = []
 col1, col2 = st.columns([1,3])
 with col1: region = st.selectbox('Choose a region?', ('Nasdaq', 'Vietnam'))
-with col2: company = st.text_input("Search companies by name or symbol", value="", suggestions=df_search)
+with col2: company = st.multiselect("Search companies by name or symbol", value="")
 
 if region == "Nasdaq":
   df = pd.read_csv('./search_engine_nasdaq.csv')
