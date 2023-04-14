22af29b0e515a8abe8b6e09a5e10829ebcf43907
hdungie <120076145+hdungie@users.noreply.github.com>
2023-04-14 12:04:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit as st
+import datetime
 
 region = st.selectbox('Choose a region?', ('Nasdaq', 'Vietnam'))
 
 company = st.text_input("Search companies by name or ticker", value="")
 
 interval = st.slider('Choose an interval', min_value = 1, max_value = 365, step = 1)
+
+start_date = st.date_input( "Start date: ")
