da565b6f02d731474a1539a963e1b4c89710df7c
hdungie <120076145+hdungie@users.noreply.github.com>
2023-04-14 15:40:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,18 @@ col1, col2 = st.columns([1,3])
 with col1: region = st.selectbox('Choose a region?', ('Nasdaq', 'Vietnam'))
 with col2: company = st.text_input("Search companies by name or symbol", value="")
 
+if region == "Nasdaq":
+  df = pd.read_csv('./companies_search_engine - nasdaq.csv')
+else: df = pd.read_csv('./companies_search_engine - vn.csv')
+
+m1 = df['symbol'].str.contains(company)
+m2 = df['company_name'].str.contains(company)
+
+df_search = df[m1 | m2]
+
+if company:
+    st.write(df_search)
+
 col1, col2 = st.columns(2)
 with col1: start_date = st.date_input( "Start date: ")
 with col2: end_date = st.date_input("End date: ")
