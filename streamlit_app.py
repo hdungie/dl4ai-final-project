55fded06b52815a06dd7b93a9e6b50285142b325
hdungie <120076145+hdungie@users.noreply.github.com>
2023-04-16 00:26:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,10 +106,10 @@ if predict_button:
   # Create the line graph
 #   fig = px.line(df[:future-1], x='Dates', y='Close price', color_discrete_sequence = ['red'],title=f'Close price prediction of {comp[0]} in {future} days')
 #   fig.update_traces(mode='markers+lines', marker=dict(size=4))
-fig = px.scatter(df[:future-1], x='Dates', y='Close price', 
-                 color='Close price', color_continuous_scale='reds',
-                 line_color='black', marker_color='white', 
-                 trendline='ols', trendline_color_override='gray'
+  fig = px.scatter(df[:future-1], x='Dates', y='Close price', 
+                   color='Close price', color_continuous_scale='reds',
+                   line_color='black', marker_color='white', 
+                   trendline='ols', trendline_color_override='gray'
 
   # Set the graph title and axis labels
 #   fig.update_layout(title=f"Close price prediction of {comp[0]} in {future} days",
