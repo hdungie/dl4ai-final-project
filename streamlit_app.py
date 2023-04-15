201dffb50642c390c1e8cfd5b092805d3acb5e3d
hdungie <120076145+hdungie@users.noreply.github.com>
2023-04-15 22:44:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,7 @@ with col2:
 col1, col2 = st.columns(2)
 with col1: start_date = st.date_input( "Start date: ")
 with col2: end_date = st.date_input("End date: ")
+st.write(type(start_date))
   
 if end_date < start_date:
     st.warning("End date must be after start date.", icon = "❌")
