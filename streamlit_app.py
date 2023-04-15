beba4fa54ec96fa3736ee1cb5d3362ff7029004e
hdungie <120076145+hdungie@users.noreply.github.com>
2023-04-15 23:28:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,46 +104,46 @@ df['Dates'] = df['Dates'].astype(str)
 
 graph = plt.figure(figsize=(16, 8), dpi=300)
 plt.plot(df['Dates'][:future-1], df['Close price'][:future-1], label='Predicted price')
-plt.title(f'Close price prediction of {comp[2]} in {future} days')
+plt.title(f'Close price prediction of {comp[0]} in {future} days')
 plt.ylabel('Close price in $')
 plt.xlabel('Dates')
 plt.legend()
 
 if predict_button:
 #   fig_html = mpld3.fig_to_html(graph)
-#   components.html(fig_html, height=600)
-# css = """
-# table
-# {
-#   border-collapse: collapse;
-# }
-# th
-# {
-#   color: #ffffff;
-#   background-color: #000000;
-# }
-# td
-# {
-#   background-color: #cccccc;
-# }
-# table, th, td
-# {
-#   font-family:Arial, Helvetica, sans-serif;
-#   border: 1px solid black;
-#   text-align: right;
-# }
-# """
-# for axes in graph.axes:
-#   for line in axes.get_lines():
-#       # get the x and y coords
-#       xy_data = line.get_xydata()
-#       labels = []
-#       for x, y in xy_data:
-#           # Create a label for each point with the x and y coords
-#           html_label = f'<table border="1" class="dataframe"> <thead> <tr style="text-align: right;"> </thead> <tbody> <tr> <th>x</th> <td>{x}</td> </tr> <tr> <th>y</th> <td>{y}</td> </tr> </tbody> </table>'
-#           labels.append(html_label)
-#       # Create the tooltip with the labels (x and y coords) and attach it to each line with the css specified
-#       tooltip = plugins.PointHTMLTooltip(line, labels, css=css)
-#       # Since this is a separate plugin, you have to connect it
-#       plugins.connect(two_subplot_fig, tooltip)
+  components.html(fig_html, height=600)
+css = """
+table
+{
+  border-collapse: collapse;
+}
+th
+{
+  color: #ffffff;
+  background-color: #000000;
+}
+td
+{
+  background-color: #cccccc;
+}
+table, th, td
+{
+  font-family:Arial, Helvetica, sans-serif;
+  border: 1px solid black;
+  text-align: right;
+}
+"""
+for axes in graph.axes:
+  for line in axes.get_lines():
+      # get the x and y coords
+      xy_data = line.get_xydata()
+      labels = []
+      for x, y in xy_data:
+          # Create a label for each point with the x and y coords
+          html_label = f'<table border="1" class="dataframe"> <thead> <tr style="text-align: right;"> </thead> <tbody> <tr> <th>x</th> <td>{x}</td> </tr> <tr> <th>y</th> <td>{y}</td> </tr> </tbody> </table>'
+          labels.append(html_label)
+      # Create the tooltip with the labels (x and y coords) and attach it to each line with the css specified
+      tooltip = plugins.PointHTMLTooltip(line, labels, css=css)
+      # Since this is a separate plugin, you have to connect it
+      plugins.connect(two_subplot_fig, tooltip)
   st.write(graph)
