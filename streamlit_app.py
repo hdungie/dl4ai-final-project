fd35b3ccb3fd2d85d1b9ec98113ef9446248fdb3
hdungie <120076145+hdungie@users.noreply.github.com>
2023-04-15 20:15:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ with col1: start_date = st.date_input( "Start date: ")
 with col2: end_date = st.date_input("End date: ")
   
 # if end_date > start_date:
-    interval = (end_date - start_date).days
-    st.warning(f"Interval: {interval} days")
+interval = (end_date - start_date).days
+st.warning(f"Interval: {interval} days")
 if end_date < start_date:
     st.warning("End date must be after start date.", icon = "❌")
   
