48cc5110c46ceba67a3dd9b1693adf3f94936708
hdungie <120076145+hdungie@users.noreply.github.com>
2023-04-15 23:42:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -156,6 +156,6 @@ if predict_button:
   # Convert the plot to HTML and display it
 #   mpld3.display(fig)
         
-  fig_html = mpld3.fig_to_html(graph)
+  fig_html = mpld3.fig_to_html(fig)
   components.html(fig_html, height=600)
   st.pyplot(fig)
