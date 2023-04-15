21e1a6853b4f31f5b15901f58732f5c3e76d6b0c
hdungie <120076145+hdungie@users.noreply.github.com>
2023-04-15 20:28:55 +0700

    Update predictions.py

--- predictions.py
+++ predictions.py
@@ -6,7 +6,8 @@ from tensorflow.keras.models import load_model
 # if streamlit_app.interval < 30:
 window_size = 30
 
-data = pd.read_csv(streamlit_app.file_path)
+def get_predictions(file_path):
+    data = pd.read_csv(file_path)
 df = data[['Date', 'Close']]
 
 new_data = []
