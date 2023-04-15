cf46b59e12d15827843c4365ddc104cfe57ae758
hdungie <120076145+hdungie@users.noreply.github.com>
2023-04-16 00:38:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,9 +104,10 @@ df['Dates'] = df['Dates'].astype(str)
 
 if predict_button:
   # Create the line graph
-  fig = px.line(df[:future-1], x='Dates', y='Close price', theme = 'streamlit', title = f'Predicted close price of {company[0]} in {future} days')
+  fig = px.line(df[:future-1], x='Dates', y='Close price', marker = True, title = f'Predicted close price of {company[0]} in {future} days')
   fig.add_trace(px.scatter(df[:future-1], x='Dates', y='Close price', 
                           color_continuous_scale='reds').data[0])
+  fig.update_traces(textposition="top")
 
   # Show the graph
   st.plotly_chart(fig)
