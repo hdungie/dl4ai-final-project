49a39e10fa9ab74ebac68e6210ba54838c50aeff
hdungie <120076145+hdungie@users.noreply.github.com>
2023-04-16 11:49:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,7 +100,7 @@ if predict_button:
   from datetime import datetime, timedelta
   df = pd.DataFrame(y_pred_denorm[-1], columns = ['Close price'])
 
-  latest = new_df.loc[len(new_df)-1,'Dates']
+  latest = new_df.loc[len(new_df)-1,'Date']
   latest = datetime.strptime(latest, '%d-%m-%Y').date()
   gap_end = end_date - latest
   gap_start = start_date - latest
