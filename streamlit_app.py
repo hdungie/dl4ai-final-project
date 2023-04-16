934ca3e7c1fed91bcb6ca0f91b5fd346bc9ff9dc
hdungie <120076145+hdungie@users.noreply.github.com>
2023-04-16 11:38:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,64 +61,64 @@ else:
   with col3 :
     predict_button = st.button("Predict")
 
-if interval < 30:
-    window_size = 150
-future = interval
-    
-data = pd.read_csv(filepath)
-new_df = data[['Date', 'Close']]
-
-new_data = []
-for i in range(1, len(new_df) - window_size - 1):
-    data_predict = []
-    # Get a window_size time frame for data feature
-    for j in range(window_size):
-        data_predict.append(new_df.loc[i + j, 'Close'])
-    new_data.append(np.array(data_predict).reshape(window_size, 1))
-
-new_data = np.array(new_data)
-# Reshape the array to have shape (number of sequences, window_size, 1)
-new_data = new_data.reshape(new_data.shape[0], window_size, 1)
-
-new_data_norm = new_data.copy()
-for i in range(0, len(new_data_norm)):
-    min_feature = np.min(new_data[i])
-    max_feature = np.max(new_data[i])
-    new_data_norm[i] = (new_data[i] - min_feature) / (max_feature - min_feature)
-
-model = load_model('./nasdaq-model-30d.h5')
-# Get prediction on the test data
-y_pred_norm = model.predict(new_data_norm)
-
-# Convert the result back to stock price (i.e., de-normalization) for visualization purpose
-y_pred_denorm = y_pred_norm
-for i in range(0, len(y_pred_denorm)): # denorm_x = norm_x * (max(x) - min(x)) + min(x)
-    y_pred_denorm[i] = y_pred_norm[i] * (max_feature - min_feature) + min_feature
-
-from datetime import datetime, timedelta
-df = pd.DataFrame(y_pred_denorm[-1], columns = ['Close price'])
-st.write(df)
-
-latest = data.loc[len(data)-1,'Dates']
-latest = datetime.strptime(latest, '%d-%m-%Y').date()
-gap_end = end_date - latest
-gap_start = start_date - latest
-
-dates = []
-current_date = latest
-for i in range(future):
-    next_date = current_date + timedelta(days=1)
-    dates.append(next_date)
-    current_date = next_date
-    
-for i in range(len(dates)):
-    dates[i] = dates[i].strftime('%d %b %Y')
-    
-df['Dates'] = pd.DataFrame(dates, columns = ['Dates'])
-df['Dates'] = df['Dates'].astype(str)
-
-close_prices = df['Close price'].apply("{:.2f}".format).tolist()
 if predict_button:
+  if interval < 30:
+      window_size = 150
+  future = interval
+
+  data = pd.read_csv(filepath)
+  new_df = data[['Date', 'Close']]
+
+  new_data = []
+  for i in range(1, len(new_df) - window_size - 1):
+      data_predict = []
+      # Get a window_size time frame for data feature
+      for j in range(window_size):
+          data_predict.append(new_df.loc[i + j, 'Close'])
+      new_data.append(np.array(data_predict).reshape(window_size, 1))
+
+  new_data = np.array(new_data)
+  # Reshape the array to have shape (number of sequences, window_size, 1)
+  new_data = new_data.reshape(new_data.shape[0], window_size, 1)
+
+  new_data_norm = new_data.copy()
+  for i in range(0, len(new_data_norm)):
+      min_feature = np.min(new_data[i])
+      max_feature = np.max(new_data[i])
+      new_data_norm[i] = (new_data[i] - min_feature) / (max_feature - min_feature)
+
+  model = load_model('./nasdaq-model-30d.h5')
+  # Get prediction on the test data
+  y_pred_norm = model.predict(new_data_norm)
+
+  # Convert the result back to stock price (i.e., de-normalization) for visualization purpose
+  y_pred_denorm = y_pred_norm
+  for i in range(0, len(y_pred_denorm)): # denorm_x = norm_x * (max(x) - min(x)) + min(x)
+      y_pred_denorm[i] = y_pred_norm[i] * (max_feature - min_feature) + min_feature
+
+  from datetime import datetime, timedelta
+  df = pd.DataFrame(y_pred_denorm[-1], columns = ['Close price'])
+  st.write(df)
+
+  latest = data.loc[len(data)-1,'Dates']
+  latest = datetime.strptime(latest, '%d-%m-%Y').date()
+  gap_end = end_date - latest
+  gap_start = start_date - latest
+
+  dates = []
+  current_date = latest
+  for i in range(future):
+      next_date = current_date + timedelta(days=1)
+      dates.append(next_date)
+      current_date = next_date
+
+  for i in range(len(dates)):
+      dates[i] = dates[i].strftime('%d %b %Y')
+
+  df['Dates'] = pd.DataFrame(dates, columns = ['Dates'])
+  df['Dates'] = df['Dates'].astype(str)
+
+  close_prices = df['Close price'].apply("{:.2f}".format).tolist()
   # Create the line graph
   fig = px.line(df[gap_start:gap_end], x='Dates', y='Close price', markers = True, title = f'Predicted close price of {ticker} from {start_date} to {end_date}', text = close_prices[:future-1])
   fig.add_trace(px.scatter(df[gap_start:gap_end], x='Dates', y='Close price',
