23b0e14dff2db36e735aa8ed7494e94b76e1c512
hdungie <120076145+hdungie@users.noreply.github.com>
2023-05-23 14:21:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ with col1:
   else:
     start_date = st.date_input( "Start date: ")
 with col2: 
-    if region == 'Nasdaq':
+  if region == 'Nasdaq':
     end_date = st.date_input( "Start date: ", min_value=datetime(2022,12,13), max_value=datetime(2023,12,13))
   if region == 'Vietnam':
     end_date = st.date_input( "Start date: ", min_value=datetime(2023,2,28), max_value=datetime(2023,8,28))
