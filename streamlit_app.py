5c76ea668062982d2c7cb0c909cb798976aa0ecd
hdungie <120076145+hdungie@users.noreply.github.com>
2023-05-23 14:22:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,11 +41,11 @@ with col1:
     start_date = st.date_input( "Start date: ")
 with col2: 
   if region == 'Nasdaq':
-    end_date = st.date_input( "Start date: ", min_value=datetime(2022,12,13), max_value=datetime(2023,12,13))
+    end_date = st.date_input( "End date: ", min_value=datetime(2022,12,13), max_value=datetime(2023,12,13))
   if region == 'Vietnam':
-    end_date = st.date_input( "Start date: ", min_value=datetime(2023,2,28), max_value=datetime(2023,8,28))
+    end_date = st.date_input( "End date: ", min_value=datetime(2023,2,28), max_value=datetime(2023,8,28))
   else:
-    end_date = st.date_input( "Start date: ")
+    end_date = st.date_input( "End date: ")
 
 comp = company.split(' ')
 ticker = comp[0]
