254cb38eced3cb33f788b99375b36015295a6f86
hdungie <120076145+hdungie@users.noreply.github.com>
2023-05-24 13:14:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -280,7 +280,7 @@ with tab3:
     with col5:
       pass
     with col3 :
-        predict_button = st.button('Predict', disabled = True)
+        predict_button = st.button('Generate', disabled = True, key = 0)
     
   col1, col2 = st.columns([1,3])
   with col1:
