e732884baa802f2cb67e0eb14d9f7a8f5967bdc7
hdungie <120076145+hdungie@users.noreply.github.com>
2023-05-24 14:10:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -287,7 +287,7 @@ with tab3:
     fin = pd.DataFrame(columns = ['scores','action'])
     fin['scores'] = scores
     fin['action']=action
-    fig = px.bar(df, x="scores", y="action", orientation='h')
+    fig = px.bar(fin, x="scores", y="action", orientation='h')
     st.plotly_chart(fig)
   with col1:
     pass
